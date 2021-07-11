a2f4e25b30249b1f19affffe9912c99bb1b56d2c
joka42 <jas.nombre@web.de>
2021-07-11 19:06:06 +0200

    mvp working and cleaned

--- selfmanipulator.py
+++ selfmanipulator.py
@@ -4,8 +4,8 @@ import sys
 import subprocess
 
 
-WAIT_TIME = 5.0
-VERSION = "0.3.8"
+WAIT_TIME = 30.0
+VERSION = "1.0.0"
 
 while True:
     print("VERSION:", VERSION)
@@ -25,35 +25,3 @@ while True:
     upgrade_process.communicate()    
     os.execv(sys.executable, ['python'] + sys.argv)
     exit()
-    
-
-
-#result = ""
-#process = subprocess.Popen(["cat", "selfmanipulator.py"])
-#process.communicate()
-
-#print(process.stdout)
-#os.system("git remote update")
-#os.system("git status -uno")
-
-
-
-"""
-
-with open("file1.txt", "w+") as file:
-    file.write("Hello this")
-
-with open("selfmanipulator.py", "r") as file:
-    lines = file.readlines()
-
-lines[5] = "    file.write(\"Hello this\")\n"
-lines[17] = "if False:\n"
-
-with open("selfmanipulator.py", "w") as file:
-    file.writelines(lines)
-
-time.sleep(3.0)
-if False:
-    os.execv(sys.executable, ['python'] + sys.argv)
-exit()
-"""
