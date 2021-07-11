c333eb3b35372fc74d689e5b112160d16f484fe3
joka42 <jas.nombre@web.de>
2021-07-11 19:09:00 +0200

    removed verbosity

--- selfmanipulator.py
+++ selfmanipulator.py
@@ -4,19 +4,18 @@ import sys
 import subprocess
 
 
-WAIT_TIME = 30.0
+WAIT_TIME = 15.0
 VERSION = "1.0.0"
 
-while True:
-    print("VERSION:", VERSION)
+print("VERSION:", VERSION)
 
+while True:
     update_process = subprocess.Popen(["git", "remote", "update"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = update_process.communicate()
     check_process = subprocess.Popen(["git", "status", "-uno"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     check, error = check_process.communicate()
     
     if "behind" not in str(check):
-        print("No update available")
         time.sleep(WAIT_TIME)
         continue
     
